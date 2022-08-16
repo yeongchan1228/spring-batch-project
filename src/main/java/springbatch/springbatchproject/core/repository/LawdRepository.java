8f3323b79abc361ee26bfd5fad2f1ac864038816
최영찬 <gks54410@gmail.com>
2022-08-16 16:14:22 +0900

    Feat: DB에 저장된 정보를 바탕으로 API를 호출하는 2단계 Step으로 구성된 Job 구현

--- src/main/java/springbatch/springbatchproject/core/repository/LawdRepository.java
+++ src/main/java/springbatch/springbatchproject/core/repository/LawdRepository.java
@@ -1,11 +1,17 @@
 package springbatch.springbatchproject.core.repository;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import springbatch.springbatchproject.core.entity.Lawd;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LawdRepository extends JpaRepository<Lawd, Long> {
 
     Optional<Lawd> findByLawdCd(String lawdCd);
+
+    @Query("select distinct substring(l.lawdCd, 1, 5) from Lawd l where l.exist=true and l.lawdCd not like '%00000000'")
+    List<String> findDistinctSubstringLawdCd(Pageable pageable);
 }
--- src/main/java/springbatch/springbatchproject/job/apt/AptDealInsertJobConfig.java
+++ src/main/java/springbatch/springbatchproject/job/apt/AptDealInsertJobConfig.java
@@ -5,22 +5,26 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.CompositeJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.xml.StaxEventItemReader;
 import org.springframework.batch.item.xml.builder.StaxEventItemReaderBuilder;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import springbatch.springbatchproject.adapter.ApartmentApiResource;
+import springbatch.springbatchproject.core.repository.LawdRepository;
 import springbatch.springbatchproject.job.apt.dto.AptDealDto;
-import springbatch.springbatchproject.job.validator.LawdCdParameterValidator;
 import springbatch.springbatchproject.job.validator.YearMonthParameterValidator;
 
 import java.time.YearMonth;
@@ -31,23 +35,30 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AptDealInsertJobConfig {
 
+    private final LawdRepository lawdRepository;
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
     private final ApartmentApiResource apartmentApiResource;
 
     @Bean
-    public Job aptDealInsertJob(Step aptDealInsertStep) {
+    public Job aptDealInsertJob(Step aptDealInsertStep,
+                                Step lawdCdReadStep,
+                                Step contextPrintStep) {
         return jobBuilderFactory.get("aptDealInsertJob")
                 .incrementer(new RunIdIncrementer())
 //                .validator(new FilePathParameterResourceValidator())
                 .validator(aptDealJobParameterValidator())
-                .start(aptDealInsertStep)
+                .start(lawdCdReadStep)
+                .next(aptDealInsertStep)
                 .build();
     }
 
     private JobParametersValidator aptDealJobParameterValidator() {
         CompositeJobParametersValidator validator = new CompositeJobParametersValidator();
-        validator.setValidators(List.of(new YearMonthParameterValidator(), new LawdCdParameterValidator()));
+        validator.setValidators(List.of(
+                new YearMonthParameterValidator()
+//                new LawdCdParameterValidator()
+        ));
         return validator;
     }
 
@@ -62,11 +73,47 @@ public class AptDealInsertJobConfig {
     }
 
     @Bean
+    @JobScope
+    public Step lawdCdReadStep() {
+        return stepBuilderFactory.get("lawdCdReadStep")
+                .tasklet((contribution, chunkContext) -> {
+                    StepExecution stepExecution = chunkContext.getStepContext().getStepExecution();
+                    ExecutionContext jobExecutionContext = stepExecution.getJobExecution().getExecutionContext();
+
+                    List<String> lawdCds = lawdRepository.findDistinctSubstringLawdCd(PageRequest.of(0, 40));
+                    jobExecutionContext.putString("lawdCd", lawdCds.get(0));
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    @JobScope
+    public Step contextPrintStep(
+            @Value("#{jobExecutionContext['lawdCd']}") String valueLawdCd
+    ) {
+        return stepBuilderFactory.get("contextPrintStep")
+                .tasklet((contribution, chunkContext) -> {
+                    ExecutionContext jobExecutionContext
+                            = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext();
+
+                    String lawdCd = jobExecutionContext.getString("lawdCd");
+                    System.out.println("lawdCd = " + lawdCd);
+                    System.out.println("valueLawdCd = " + valueLawdCd);
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+
+    }
+
+    @Bean
     @StepScope
     public StaxEventItemReader<AptDealDto> aptDealResourceReader(
 //            @Value("#{jobParameters['filePath']}") String filePath,
             @Value("#{jobParameters['yearMonth']}") String yearMonth,
-            @Value("#{jobParameters['lawdCd']}") String lawdCd,
+//            @Value("#{jobParameters['lawdCd']}") String lawdCd,
+            @Value("#{jobExecutionContext['lawdCd']}") String lawdCd,
             Jaxb2Marshaller aptDealDtoMarshaller
     ) {
         return new StaxEventItemReaderBuilder<AptDealDto>()
